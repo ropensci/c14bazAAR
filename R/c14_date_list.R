6ad8962f4047a9072334a25ef49d8c778bf084d2
nevrome <clemens@nevrome.de>
2017-07-30 22:01:41 +0200

    added variable order function for c14_date_lists

--- R/c14_date_list.R
+++ R/c14_date_list.R
@@ -1,23 +1,27 @@
-#' Checks if a variable is of class c14_date_list
+#### is ####
+
+#' Check if a variable is of class c14_date_list
 #'
-#' Checks if a variable is of class c14_date_list
+#' Check if a variable is of class c14_date_list
 #'
 #' @param x a variable
-#' @param ... further arguments passed to or from other methods.
+#' @param ... further arguments passed to or from other methods
 #'
 #' @return true if x is a c14_date_list, false otherwise
 #'
 #' @export
 is.c14_date_list <- function(x, ...) {"c14_date_list" %in% class(x)}
 
+#### format ####
+
 #' Encode a c14_date_list in a Common Format
 #'
-#' Format an c14_date_list for pretty printing.
+#' Format an c14_date_list for pretty printing
 #'
 #' @param x a c14_date_list
-#' @param ... further arguments passed to or from other methods.
+#' @param ... further arguments passed to or from other methods
 #'
-#' @return A string representation of the c14_date_list.
+#' @return A string representation of the c14_date_list
 #'
 #' @export
 format.c14_date_list <- function(x, ...) {
@@ -46,12 +50,14 @@ format.c14_date_list <- function(x, ...) {
   invisible(return_value)
 }
 
+#### print ####
+
 #' Print a c14_date_list
 #'
 #' Print a c14_date_list according to the format.c14_date_list
 #'
 #' @param x a c14_date_list
-#' @param ... further arguments passed to or from other methods.
+#' @param ... further arguments passed to or from other methods
 #'
 #' @export
 print.c14_date_list <- function(x, ...) {
@@ -59,4 +65,46 @@ print.c14_date_list <- function(x, ...) {
   cat(format(x, ...), "\n\n")
   # add table printed like a tibble
   x %>% `class<-`(c("tbl", "tbl_df", "data.frame")) %>% print
+}
+
+#### order variables ####
+
+#' @name order_variables
+#' @title Order variables (static key)
+#'
+#' @description Order variables in a c14_date_list after a defined key
+#'
+#' @param x an object of class c14_date_list
+#'
+#' @return an object of class c14_date_list
+#' @export
+#'
+#' @rdname order_variables
+#'
+order_variables <- function(x) {
+  UseMethod("order_variables")
+}
+
+#' @rdname order_variables
+#' @export
+order_variables.default <- function(x) {
+  stop("x is not an object of class c14_date_list")
+}
+
+#' @rdname order_variables
+#' @export
+order_variables.c14_date_list <- function(x) {
+  # apply ordering key
+  x %>%
+    dplyr::select(
+      .data[["labnr"]],
+      .data[["site"]],
+      .data[["c14age"]],
+      .data[["c14std"]],
+      .data[["material"]],
+      .data[["lat"]],
+      .data[["lon"]],
+      dplyr::everything()
+    ) %>%
+    return()
 }
