6132208cddb2efc7d77fa8fde176ac559870534a
nevrome <clemens@nevrome.de>
2017-07-30 16:08:55 +0200

    added dummies for other possible databases

---
+++ R/14sea.R
@@ -0,0 +1,14 @@
+#' get current 14SEA-Database
+#'
+#' Downloads the current version of the 14SEA-Database from \url{http:///www.14sea.org/}.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'   14SEA <- get_14SEA()
+#' }
+#'
+#' @export
+get_14SEA <- function() {
+  "Not yet implemented."
+}
---
+++ R/card.R
@@ -0,0 +1,14 @@
+#' get current CARD-Database [no open API]
+#'
+#' Downloads the current version of the CARD-Database from \url{http://www.canadianarchaeology.ca/}.
+#'
+#' @examples
+#'
+#' \dontrun{
+#'   CARD <- get_CARD()
+#' }
+#'
+#' @export
+get_CARD <- function() {
+  "Data not available."
+}
