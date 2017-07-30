33b49bd814f448e7ac58daed0a69903315d479d0
nevrome <clemens@nevrome.de>
2017-07-30 23:33:08 +0200

    calibration function and small necessary changes in order variables function

--- R/c14_date_list.R
+++ R/c14_date_list.R
@@ -94,17 +94,141 @@ order_variables.default <- function(x) {
 #' @rdname order_variables
 #' @export
 order_variables.c14_date_list <- function(x) {
-  # apply ordering key
-  x %>%
-    dplyr::select(
-      .data[["labnr"]],
-      .data[["site"]],
-      .data[["c14age"]],
-      .data[["c14std"]],
-      .data[["material"]],
-      .data[["lat"]],
-      .data[["lon"]],
-      dplyr::everything()
+
+  # apply ordering key but consider special case calage & calstd
+  if (c("calage", "calstd") %in% colnames(x) %>% all) {
+    x <- x %>%
+      dplyr::select(
+        .data[["labnr"]],
+        .data[["site"]],
+        .data[["c14age"]],
+        .data[["c14std"]],
+        .data[["calage"]],
+        .data[["calstd"]],
+        .data[["material"]],
+        .data[["lat"]],
+        .data[["lon"]],
+        dplyr::everything()
+      )
+  } else {
+    x <- x %>%
+      dplyr::select(
+        .data[["labnr"]],
+        .data[["site"]],
+        .data[["c14age"]],
+        .data[["c14std"]],
+        .data[["material"]],
+        .data[["lat"]],
+        .data[["lon"]],
+        dplyr::everything()
+      )
+  }
+
+  return(x)
+}
+
+#### calibrate ####
+
+#' @name calibrate
+#' @title Calibrate dates
+#'
+#' @description Calibrate all valid dates in a c14_date_list
+#'
+#' @param x an object of class c14_date_list
+#'
+#' @return an object of class c14_date_list
+#' @export
+#'
+#' @rdname calibrate
+#'
+calibrate <- function(x) {
+  UseMethod("calibrate")
+}
+
+#' @rdname calibrate
+#' @export
+calibrate.default <- function(x) {
+  stop("x is not an object of class c14_date_list")
+}
+
+#' @rdname calibrate
+#' @export
+calibrate.c14_date_list <- function(x) {
+
+  if (nrow(x) > 1000) {
+    message("This may take several minutes...")
+  }
+
+  # setup progress bar
+  pb <- utils::txtProgressBar(
+    max = 100,
+    style = 3
+  )
+
+  # add or empty columns calage and calstd
+  if (c("calage", "calstd") %in% colnames(x) %>% all) {
+    x$calage <- NA
+    x$calstd <- NA
+  } else {
+    x <- x %>%
+      tibble::add_column(
+        calage = NA,
+        .after = "c14std"
+      ) %>%
+      tibble::add_column(
+        calstd = NA,
+        .after = "calage"
+      )
+  }
+
+  # determine dates that are out of the range of the calcurve can not be calibrated
+  toosmall <- which(x$c14age < 100)
+  toobig <- which(x$c14age > 45000)
+  outofrange <- c(toosmall, toobig) %>% unique
+
+  # copy these dates without calibration
+  x$calage[outofrange] <- x$c14age[outofrange]
+  x$calstd[outofrange] <- x$c14std[outofrange]
+
+  # increment progress bar
+  utils::setTxtProgressBar(pb, 5)
+
+  # 2sigma range probability threshold
+  threshold <- (1 - 0.9545) / 2
+
+  # calibration
+  interval95 <- Bchron::BchronCalibrate(
+      ages      = x$c14age[-outofrange],
+      ageSds    = x$c14std[-outofrange],
+      calCurves = rep("intcal13", nrow(x[-outofrange, ])),
+      eps       = 1e-06
     ) %>%
-    return()
+    # extract border ages of the 2sigma range
+    plyr::ldply(., function(x) {
+        x$densities            %>% cumsum -> a      # cumulated density
+        which(a <= threshold)  %>% max    -> my_min # lower border
+        which(a > 1-threshold) %>% min    -> my_max # upper border
+        x$ageGrid[c(my_min, my_max)]
+      }
+    )
+
+  # increment progress bar
+  utils::setTxtProgressBar(pb, 95)
+
+  # take the mean of the borders as CALAGE and the distance
+  # of CALAGE to the upper and lower 95.45% interval as CALSTD
+  top <- round(interval95[, 3])
+  amean <- apply(interval95[, 2:3], 1, function(x){round(mean(x))})
+
+  # write result back into x
+  x$calage[-outofrange] <- amean
+  x$calstd[-outofrange] <- top - amean
+
+  # increment progress bar
+  utils::setTxtProgressBar(pb, 100)
+
+  # close progress bar
+  close(pb)
+
+  return(x)
 }
