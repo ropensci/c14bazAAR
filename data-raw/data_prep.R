946f9ab4af30f5ccd8551ea304e69b4c085e6f70
nevrome <clemens@nevrome.de>
2017-08-04 14:35:01 +0200

    replaced unknown in thesauri by NA

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,9 +1,11 @@
 country_thesaurus <- readr::read_csv(
   "data-raw/country_thesaurus.csv",
+  na = c("NA")
 )
 devtools::use_data(country_thesaurus, overwrite = TRUE)
 
 material_thesaurus <- readr::read_csv(
   "data-raw/material_thesaurus.csv",
+  na = c("NA")
 )
 devtools::use_data(material_thesaurus, overwrite = TRUE)
