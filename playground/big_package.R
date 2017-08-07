1572e8609f76993a719f06dd41ef5e9d52fba032
nevrome <clemens@nevrome.de>
2017-08-07 14:21:36 +0200

    more playing around with the complete workflow

--- playground/big_package.R
+++ playground/big_package.R
@@ -12,3 +12,12 @@ a4 <- a3 %>% calibrate()
 a5 <- a4 %>% thesaurify()
 
 a6 <- a5 %>% estimate_spatial_quality()
+
+a7 <- a6 %>% order_variables()
+
+save(a1, a2, a3, a4, a5, a6, a7, file = "playground/dates.RData")
+
+a7 %>%
+  dplyr::filter(
+    is.na(country_thes) & !is.na(country)
+  ) -> hu
