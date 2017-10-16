7578668b494a2f4c5e2e3caaf55512ecd43b6bbb
nevrome <clemens@nevrome.de>
2017-10-16 22:47:19 +0200

    started to work on getter function for 14SEA

--- R/14sea.R
+++ R/14sea.R
@@ -10,5 +10,81 @@
 #'
 #' @export
 get_14SEA <- function() {
-  "Not yet implemented."
+
+  # URL
+  db_url <- "http://www.14sea.org/img/14SEA_Full_Dataset_2017-01-29.xlsx"
+
+  # check connection
+  if (!RCurl::url.exists(db_url)) {stop(paste(db_url, "is not available. No internet connection?"))}
+
+  # download data to temporary file
+  tempo <- tempfile()
+  utils::download.file(db_url, tempo)
+
+  # read data
+  SEA14 <- tempo %>%
+    readxl::read_xlsx(
+      trim_ws = TRUE,
+      na = c("Combination fails", "nd", "-"),
+      col_types = c(
+        Site = "text",
+        Subregion = "text",
+        `Subregion no.` = "text",
+        Country = "text",
+        `Lab. no.` = "text",
+        `Date BP` = "text",
+        `±` = "text",
+        δ13C = "text",
+        `calBC 1σ (from)` = "text",
+        `calBC 1σ (to)` = "text",
+        Material = "text",
+        Level = "text",
+        Provenance = "text",
+        Comment = "text",
+        Period = "text",
+        `Reference 1` = "text",
+        `Reference 2` = "text",
+        `Reference 3` = "text",
+        `Reference 4` = "text"
+      )
+    )# %>%
+    # dplyr::rename(
+    #   id = .data[["ID"]],
+    #   labnr = .data[["LABNR"]],
+    #   c14age = .data[["C14AGE"]],
+    #   c14std = .data[["C14STD"]],
+    #   c13val = .data[["C13"]],
+    #   material = .data[["MATERIAL"]],
+    #   species = .data[["SPECIES"]],
+    #   country = .data[["COUNTRY"]],
+    #   site = .data[["SITE"]],
+    #   period = .data[["PERIOD"]],
+    #   culture = .data[["CULTURE"]],
+    #   featuretype = .data[["FEATURETYPE"]],
+    #   feature = .data[["FEATURE"]],
+    #   lat = .data[["LATITUDE"]],
+    #   lon = .data[["LONGITUDE"]],
+    #   shortref = .data[["REFERENCE"]],
+    #   pages = .data[["PAGES"]]
+    # ) %>%
+    # # unite shortref & pages (if not NA)
+    # tidyr::replace_na(list(shortref = "", pages = "")) %>%
+    # tidyr::unite_(
+    #   ., "shortref", c("shortref", "pages"), sep = ", ", remove = TRUE
+    # ) %>%
+    # dplyr::mutate(
+    #   shortref = replace(.$shortref, which(.$shortref == ", "), NA)
+    # ) %>%
+    # dplyr::mutate(
+    #   shortref = gsub("[,]+[[:space:]]$", "", .$shortref)
+    # ) %>% dplyr::mutate(
+    #   sourcedb = "RADON"
+    # ) %>%
+    # as.c14_date_list() %>%
+    # c14databases::order_variables()
+
+    # delete temporary file
+    unlink(tempo)
+
+    return(SEA14)
 }
