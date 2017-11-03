eb082fb2b4f62b8e0d3fbe6b4203b93ee052b42c
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2017-11-03 18:12:18 +0100

    added col_type determination

--- R/eastafrica.R
+++ R/eastafrica.R
@@ -28,33 +28,43 @@ get_KITEeastAfrica <- function() {
   writeBin(get_file("CARD Upload Template - KITE East Africa v2.1.csv", "doi:10.7910/DVN/NJLNRJ"), tempo)
 
   # read data
-  KITEeastafrica <- readr::read_csv("data-raw/KITE_EastAfrica.csv",
+  KITEeastafrica <- tempo %>%
+    readr::read_csv(
       skip = 3,
       trim_ws = TRUE,
-      col_types = list(
-        'Field Number'='_',
-        'Taxa Dated'= '_',
-        'Type of Date'= '_',
+      col_types = readr::cols(
+        'Lab Number' = readr::col_character(),
+        'Field Number' = '_',
+        'Material Dated' = readr::col_character(),
+        'Taxa Dated' = '_',
+        'Type of Date' = readr::col_character(),
         'Locality' = '_',
+        'Latitude' = readr::col_double(),
+        'Longitude' = readr::col_double(),
         'Map Sheet' = '_',
-        'Elevation     (m ASL)' = '_',
+        'Elevation     (m ASL)' = readr::col_integer(),
         'Submitter' = '_',
         'Date Submitted' = '_',
-        'Collector' = '_',
+        'Collector' = readr::col_character(),
         'Date Collected' = '_',
-        'Updater' = '_',
-        'Date Updated' = '_',
+        'Updater' = readr::col_character(),
+        'Date Updated' = readr::col_date(format = "%d/%m/%Y"),
         'Measured Age' = '_',
         'MA Sigma' = '_',
-        'Delta 13 C Source' = '_',
+        'Normalized Age' = readr::col_integer(),
+        'NA Sigma' = readr::col_integer(),
+        'Delta 13C (per mil)' = readr::col_double(),
+        'Delta 13 C Source' = readr::col_character(),
         'Significance' = '_',
-        'Site Name' = '_',
+        'Site Identifier' = readr::col_character(),
+        'Site Name' = readr::col_character(),
         'Stratigraphic Component' = '_',
         'Context' = '_',
-        'Associated Taxa' = '_',
-        'Additional Information' = '_',
-        'Comments' = '_'
-        )) %>%
+        'Associated Taxa' = readr::col_character(),
+        'Additional Information' = readr::col_character(),
+        'Comments' = readr::col_character()
+      )
+    ) %>%
     dplyr::rename(
       labnr = .data[["Lab Number"]],
       c14age = .data[["Normalized Age"]],
@@ -64,7 +74,17 @@ get_KITEeastAfrica <- function() {
       site = .data[["Site Identifier"]],
       lat = .data[["Latitude"]],
       lon = .data[["Longitude"]],
-      shortref = .data[["References"]]
+      shortref = .data[["References"]],
+      type_date= .data[["Type of Date"]],
+      elevation = .data[["Elevation     (m ASL)"]],
+      collector = .data[["Collector"]],
+      updater = .data[["Updater"]],
+      date_updated = .data[["Date Updated"]],
+      delta_13c_source = .data[["Delta 13 C Source"]],
+      site_additional = .data[["Site Name"]],
+      taxa_associated = .data[["Associated Taxa"]],
+      additional_information = .data[["Additional Information"]],
+      comments = .data[["Comments"]]
     ) %>% dplyr::mutate(
       sourcedb = "KITEeastafrica"
     ) %>%
