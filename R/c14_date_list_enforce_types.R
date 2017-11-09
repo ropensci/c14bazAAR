9e636d00574022733407c5c1d016233b7ead7204
nevrome <clemens@nevrome.de>
2017-11-09 12:30:30 +0100

    added function to enforce variable types -- that will simplify the parsing process

---
+++ R/c14_date_list_enforce_types.R
@@ -0,0 +1,47 @@
+#### enforce variable types ####
+
+#' @name enforce_types
+#' @title Enforce variable types
+#'
+#' @description Enforce variable types in a c14_date_list and remove everything that doesn't fit
+#'
+#' @param x an object of class c14_date_list
+#'
+#' @return an object of class c14_date_list
+#' @export
+#'
+#' @rdname enforce_types
+#'
+enforce_types <- function(x) {
+  UseMethod("enforce_types")
+}
+
+#' @rdname enforce_types
+#' @export
+enforce_types.default <- function(x) {
+  stop("x is not an object of class c14_date_list")
+}
+
+#' @rdname enforce_types
+#' @export
+enforce_types.c14_date_list <- function(x) {
+
+  # define variable type lists
+  chr_cols <- c(
+    "sourcedb", "method", "labnr", "site", "feature", "period",
+    "culture", "material", "material_the", "species", "country",
+    "country_coord", "country_the", "spatial_quality", "shortref",
+    "comment"
+  )
+  int_cols <- c("c14age", "c14std", "calage", "calstd")
+  dbl_cols <- c("c13val", "lat", "lon")
+
+  # transform (invalid values become NA)
+  x <- x %>%
+    dplyr::mutate_if(colnames(.) %in% chr_cols, as.character) %>%
+    dplyr::mutate_if(colnames(.) %in% int_cols, as.integer) %>%
+    dplyr::mutate_if(colnames(.) %in% dbl_cols, as.double) %>%
+    as.c14_date_list()
+
+  return(x)
+}
