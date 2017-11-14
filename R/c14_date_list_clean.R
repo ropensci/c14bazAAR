705b523e6c0c24776e6c126e55fea79ba175f0d7
nevrome <clemens@nevrome.de>
2017-11-14 21:03:28 +0100

    clean can't have messages if it's called so often...

--- R/c14_date_list_clean.R
+++ R/c14_date_list_clean.R
@@ -28,11 +28,9 @@ clean.c14_date_list <- function(x) {
 
   # lat&lon not available but zero
   x[which(x[["lon"]] == 0 & x[["lat"]] == 0), c("lon", "lat")] <- NA
-  message("Made missing coordinate values explicit. 0/0 -> NA/NA.")
 
   # lat&long not on this earth
   x[which(x[["lon"]] > 180 | x[["lon"]] < -180 | x[["lat"]] > 90 | x[["lat"]] < -90), c("lon", "lat")] <- NA
-  message("Removed coordinate values which are out of bounds.")
 
   # add class again -- gets lost in in mutate_if :-(
   x <- x %>%
