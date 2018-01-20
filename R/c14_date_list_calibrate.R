0e32150b295cb2020375c6811d3de29193d06dde
nevrome <clemens@nevrome.de>
2018-01-21 00:12:08 +0100

    code refactoring to allow for faster review... wink wink nudge nudge

--- R/c14_date_list_calibrate.R
+++ R/c14_date_list_calibrate.R
@@ -26,20 +26,7 @@ calibrate.default <- function(x) {
 #' @export
 calibrate.c14_date_list <- function(x) {
 
-  # check if packages Bchron and plyr are available
-  if (
-    c("Bchron", "plyr") %>%
-    sapply(function(x) {requireNamespace(x, quietly = TRUE)}) %>%
-    all %>% `!`
-  ) {
-    stop(
-      "R packages 'Bchron' and 'plyr' needed for this function to work. Please install it.",
-      call. = FALSE
-    )
-  }
-
-  # load intcal13 data from Bchron
-  utils::data("intcal13", package = "Bchron")
+  check_if_packages_are_available(c("Bchron", "plyr"))
 
   # start message:
   message(paste0("Calibration... ", {if (nrow(x) > 1000) {"This may take several minutes."}}))
@@ -52,35 +39,12 @@ calibrate.c14_date_list <- function(x) {
     char = "+"
   )
 
-  # add or replace empty integer column calage
-  if ("calage" %in% colnames(x) %>% all) {
-    x$calage <- NA_integer_
-  } else {
-    x <- x %>%
-      tibble::add_column(
-        calage = NA_integer_,
-        .after = "c14std"
-      )
-  }
+  # add empty columns "calage" and "calprobdistr"
+  x %<>% add_or_replace_column_in_df("calage",  NA_integer_, .after = "c14std")
+  x %<>% add_or_replace_column_in_df("calprobdistr",  I(replicate(nrow(x), data.frame())), .after = "calage")
 
-  # add or replace empty list column filled with empty data.frames calprobdistr
-  if ("calprobdistr" %in% colnames(x) %>% all) {
-    x$calprobdistr <- I(replicate(nrow(x), data.frame()))
-  } else {
-    x <- x %>%
-      tibble::add_column(
-        calprobdistr = I(replicate(nrow(x), data.frame())),
-        .after = "calage"
-      )
-  }
-
-  # determine dates that are out of the range of the calcurve can not be calibrated
-  toona <- which(is.na(x$c14age) | is.na(x$c14std))
-  toosmall <- which(x$c14age < min(intcal13[,2]))
-  toobig <- which(x$c14age > max(intcal13[,2]))
-  outofrange <- c(toona, toosmall, toobig) %>% unique
-
-  # extract dates which are not out of range
+  # extract dates which are not out of range of calcurve
+  outofrange <- x %>% determine_dates_out_of_range_of_calcurve()
   calibrateable <- x[-outofrange, ]
 
   # create empty calibration result data.frame list
@@ -101,34 +65,18 @@ calibrate.c14_date_list <- function(x) {
     step_seq <- (step_width * (i - 1) + 1):if (i == amount_of_iterations)
     {nrow(calibrateable)} else {(step_width * i)}
 
-    # separate data in stacks of size step_width
-    calibrateable_step <- calibrateable[step_seq,]
-
-    # calibration
-    calibrateable_prodistr[step_seq] <- Bchron::BchronCalibrate(
-      ages      = calibrateable_step$c14age,
-      ageSds    = calibrateable_step$c14std,
-      calCurves = rep("intcal13", nrow(calibrateable_step)),
-      eps       = 1e-06
-    ) %>%
-      # extract border ages of the 2sigma range
-      lapply(
-        function(x) {
-          data.frame(calage = x[["ageGrid"]], density = x[["densities"]])
-        }
-      )
+    # calibration in stacks of size step_width
+    calibrateable_prodistr[step_seq] <- calibrate_to_probability_distribution(
+      calibrateable[step_seq, ],
+      eps = 1e-06
+    )
 
     # increment progress bar
     utils::setTxtProgressBar(pb, 5 + 90 * i/amount_of_iterations)
   }
 
   # determine calage
-  calage_vector <- calibrateable_prodistr %>%
-    lapply(
-      function(x) {
-        x$calage[which.max(x$density)]
-      }
-    ) %>% unlist
+  calage_vector <- determine_calage_from_probability_distribution(calibrateable_prodistr)
 
   # write result back into x
   x$calage[-outofrange] <- calage_vector
@@ -141,4 +89,60 @@ calibrate.c14_date_list <- function(x) {
   close(pb)
 
   return(x)
+}
+
+#' determine_dates_out_of_range_of_calcurve
+#'
+#' @param x c14_date_list
+#'
+#' @return indizes of dates in c14_date_list which are out of calcurve range
+determine_dates_out_of_range_of_calcurve <- function(x) {
+  # load intcal13 data from Bchron
+  utils::data("intcal13", package = "Bchron")
+
+  toona <- which(is.na(x$c14age) | is.na(x$c14std))
+  toosmall <- which(x$c14age < min(intcal13[,2]))
+  toobig <- which(x$c14age > max(intcal13[,2]))
+  outofrange <- c(toona, toosmall, toobig) %>% unique
+
+  return(outofrange)
+}
+
+#' calibrate_to_probability_distribution
+#'
+#' @param x c14_date_list
+#' @param ... passed to Bchron::BchronCalibrate()
+#'
+#' @return list with probability distribution data frames
+calibrate_to_probability_distribution <- function(x, ...) {
+  Bchron::BchronCalibrate(
+    ages      = x$c14age,
+    ageSds    = x$c14std,
+    calCurves = rep("intcal13", nrow(x)),
+    ...
+  ) %>%
+    # extract probability distribution from BchronCalibrate output
+    # and craft new data.frame with year and density
+    lapply(
+      function(x) {
+        data.frame(calage = x[["ageGrid"]], density = x[["densities"]])
+      }
+    ) %>%
+    return()
+}
+
+#' determine_calage_from_probability_distribution
+#'
+#' @param x c14_date_list
+#'
+#' @return vector of calages
+determine_calage_from_probability_distribution <- function(x) {
+  x %>%
+    lapply(
+      function(x) {
+        x$calage[which.max(x$density)]
+      }
+    ) %>%
+    unlist %>%
+    return()
 }
---
+++ R/general_helper_functions.R
@@ -0,0 +1,42 @@
+#' check_if_packages_are_available
+#'
+#' @param packages_ch packages that should be available
+#'
+#' @return NULL - called for side effect stop()
+check_if_packages_are_available <- function(packages_ch) {
+  if (
+    packages_ch %>%
+    sapply(function(x) {requireNamespace(x, quietly = TRUE)}) %>%
+    all %>% `!`
+  ) {
+    stop(
+      paste0(
+        "R packages ",
+        paste(packages_ch, collapse = ", "),
+        " needed for this function to work. Please install."
+      ),
+      call. = FALSE
+    )
+  }
+}
+
+#' add_or_replace_column_in_df
+#'
+#' @param x data.frame
+#' @param column_name_s name of new column
+#' @param column_content_mi content of new column
+#' @param ... passed to tibble::add_column()
+#'
+#' @return data.frame with new column
+add_or_replace_column_in_df <- function(x, column_name_s, column_content_mi, ...) {
+  if (column_name_s %in% colnames(x) %>% all) {
+    x[[column_name_s]] <- column_content_mi
+  } else {
+    x <- x %>%
+      tibble::add_column(
+        !!(column_name_s) := column_content_mi,
+        .after = "c14std"
+      )
+  }
+  return(x)
+}
