6088b86ae472dcb410dc4e5c31343ffdba6b7655
nevrome <clemens@nevrome.de>
2018-01-27 21:22:00 +0100

    added standardize_country_name()

---
+++ R/c14_date_list_standardize_country_name.R
@@ -0,0 +1,89 @@
+#### standardize_country_name ####
+
+#' @name standardize_country_name
+#' @title Apply country name standardization
+#'
+#' @description Add column country_thes with standardized country names
+#'
+#' @param x an object of class c14_date_list
+#' @param codesets which country codesets should be searched beyond "country.name.en".
+#' See \code{?countrycode::codelist} for more information.
+#' @param ... additional arguments are passed to \code{stringdist::stringdist()}.
+#' \code{stringdist()} is used for fuzzy string matching of the country names.
+#'
+#' @return an object of class c14_date_list
+#' @export
+#'
+#' @rdname standardize_country_name
+#'
+standardize_country_name <- function(
+  x,
+  codesets = c("country.name.de", "iso3c"),
+  ...
+) {
+  UseMethod("standardize_country_name")
+}
+
+#' @rdname standardize_country_name
+#' @export
+standardize_country_name.default <- function(
+  x,
+  codesets = c("country.name.de", "iso3c"),
+  ...
+) {
+  stop("x is not an object of class c14_date_list")
+}
+
+#' @rdname standardize_country_name
+#' @export
+standardize_country_name.c14_date_list <- function(
+  x,
+  codesets = c("country.name.de", "iso3c"),
+  ...
+) {
+
+  x %<>% add_or_replace_column_in_df("country_thes", NA, .after = "country")
+
+  x %<>%
+    dplyr::mutate(
+      country_thes = lookup_in_countrycode_codelist(.$country, codesets, ...)
+    ) %>%
+    as.c14_date_list()
+
+  return(x)
+}
+
+#' lookup_in_countrycode_codelist
+#'
+#' @param x vector of country codes to look up in countrycode codelist
+#' @param codesets which country codesets should be searched beyond "country.name.en"
+#' @param ... additional arguments are passed to stringdist::stringdist()
+#'
+#' @return a vector with the correct english country names
+lookup_in_countrycode_codelist <- function(x, codesets, ...){
+
+  check_if_packages_are_available(c("countrycode", "stringdist"))
+
+  codes <- unique(c("country.name.en", codesets))
+  country_df <- countrycode::codelist[, codes]
+
+  x %>% pbapply::pbsapply(
+    FUN = function(db_word) {
+      country_df %>%
+        dplyr::mutate_all(
+          dplyr::funs(stringdist = stringdist::stringdist(db_word, ., ...))
+        ) %>%
+        tidyr::gather(
+          key = "code_type",
+          value = "dist",
+          -codes
+        ) %>%
+        dplyr::slice(
+          which.min(.data$dist)
+        ) %>%
+        magrittr::extract2("country.name.en") %>%
+        magrittr::extract(1)
+    }
+  )
+
+}
