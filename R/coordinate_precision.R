afa0a20ccab8accf6e0b51981899dfaa52528df1
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-02-05 22:44:59 +0100

    add longitude info to circumference calculattor

--- R/coordinate_precision.R
+++ R/coordinate_precision.R
@@ -32,11 +32,17 @@ coordinate_precision.c14_date_list <- function(x) {
 #' circumference_calculator
 #'
 #' @param x vector of latitude
-#'
+#' @param mode lat or lon
 #' @return vecor with circumference values at specific latitudes
-circumference_calculator <- function(x, ...) {
-  circumference_equator = 2*pi*6378137 # Following WGS84
-  output = circumference_equator * cos((x*pi)/180)
+circumference_calculator <- function(x, mode, ...) {
+  if(mode == "lat") {
+    circumference <- (2 * pi) * 6378137 # Following WGS84
+  }
+  if(mode == "lon") {
+    circumferencen <- (2* pi) * 6356752.3142
+  }
+
+  output <- circumference * cos((x * pi) / 180)
   return(output)
 }
 
@@ -47,8 +53,11 @@ circumference_calculator <- function(x, ...) {
 #'
 #' @return vecor with numer of digits
 digits_counter <- function(x, ...) {
-  output=sapply(x,function(x){nchar(unlist(strsplit(as.character(x),"\\."))[2])})
-  output=ifelse(is.na(output),0,output)
+  output <- sapply(X = x,
+                   FUN = function(x){nchar(unlist(strsplit(as.character(x),"\\."))[2])})
+  output <- ifelse(test = is.na(output),
+                   yes = 0,
+                   no = output)
   return(output)
 }
 
@@ -58,7 +67,7 @@ digits_counter <- function(x, ...) {
 #'
 #' @return vecor with precision in meters
 latitude_precision <- function(x, ...) {
-  output=circumference_calculator(x)/(360*10^digits_counter(x))
+  output <- circumference_calculator(x) / (360 * 10^digits_counter(x))
   return(output)
 }
 
