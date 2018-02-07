76ef803870018e1c4ab57313bd6643caed0170a2
kschmuetz <kschmuetz@gshdl.uni-kiel.de>
2018-02-07 16:49:14 +0100

    typo in finalize function

--- R/finalize_country_name.R
+++ R/finalize_country_name.R
@@ -1,10 +1,10 @@
 #### finalize_country_name ####
 
 #' @name finalize_country_name
-#' @title Chooses the country_name, that is most likely to be correct 
+#' @title Chooses the country_name, that is most likely to be correct
 #'
 #' @description Picks the country name in a hierarchical order from the results
-#' of the determine_country_by_coords and standardize_country_name functions, 
+#' of the determine_country_by_coords and standardize_country_name functions,
 #' followed by the original input of the database
 #'
 #' @param x an object of class c14_date_list
@@ -27,15 +27,15 @@ finalize_country_name.default <- function(x) {
 #' @rdname finalize_country_name
 #' @export
 finalize_country_name.c14_date_list <- function(x) {
-  
+
   # check if the columns country_coord and country_thes are present
   necessary_vars <- c("country_coord", "country_thes")
   present <- necessary_vars %in% colnames(x)
-  
+
   if (all(present)) {
     # add the column country_final
     x %<>% add_or_replace_column_in_df("country_final", NA, .after = "country_thes")
-    
+
     # pick the most likely result hierarchically from country_coord, country_thes
     # and the original input data
     x %<>%
@@ -49,11 +49,11 @@ finalize_country_name.c14_date_list <- function(x) {
         }    ) %>%
       as.c14_date_list()
     return(x)
-    
+
   } else {
     stop(
       "The following variables (columns) are missing: ",
       paste(necessary_vars[!present], collapse = ", ")
     )
-    
+  }
 }
