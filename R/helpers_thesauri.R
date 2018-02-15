34287b85e1cbce5e7341c8a18178a999b687c98a
nevrome <clemens@nevrome.de>
2018-02-15 14:16:02 +0100

    moved helper functions useful for both thesaurification functions to helpers_thesauri.R

--- R/helpers_thesauri.R
+++ R/helpers_thesauri.R
@@ -30,3 +30,55 @@ get_thesaurus <- function(url) {
     trim_ws = TRUE
   )
 }
+
+#' print_lookup_decisions
+#'
+#' @param x a c14_date_list with country and country_thes
+#' @param variants_column name of the column with heterogeneous values
+#' @param corrected_column name of the column with the corrected values
+#' @param thesaurus data.frame with correct and variants of country names
+#'
+#' @return NULL, called for the print side effect
+print_lookup_decisions <- function(x, variants_column, corrected_column, thesaurus) {
+  changes <- find_lookup_decisions(x, variants_column, corrected_column, thesaurus)
+  message("The following decisions were made: \n")
+  for(i in 1:nrow(changes)) {
+    message(
+      ifelse(
+        changes$thesaurus[i],
+        crayon::green("thesaurus:   "),
+        ifelse(
+          changes$no_change[i],
+          crayon::red("no change:   "),
+          crayon::yellow("string match:")
+        )
+      ),
+      " ",
+      changes[[variants_column]][i], " -> ", changes[[corrected_column]][i]
+    )
+  }
+  message("\ ")
+}
+
+#' find_lookup_decisions
+#'
+#' @param x a c14_date_list
+#' @param variants_column name of the column with heterogeneous values
+#' @param corrected_column name of the column with the corrected values
+#' @param thesaurus data.frame with correct values (cor) and variants (var)
+#'
+#' @return a tibble with the country names and the new country_thes names
+#' found by \code{find_correct_name_by_stringdist_comparison()}
+find_lookup_decisions <- function(x, variants_column, corrected_column, thesaurus) {
+  x %>%
+    dplyr::select(.data[[variants_column]], .data[[corrected_column]]) %>%
+    dplyr::filter(!is.na(.data[[corrected_column]])) %>%
+    unique %>%
+    dplyr::arrange(.data[[variants_column]]) %>%
+    # check if decision was based on thesaurus entry
+    dplyr::mutate(
+      thesaurus = .data[[variants_column]] %in% thesaurus$var,
+      no_change = .data[[variants_column]] == .data[[corrected_column]]
+    )
+}
+
