3656eaa201325801c1539f677b9d589e9ba9d71b
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-02-23 15:16:31 +0100

    test euroevol; join issue due to low res of rworldxtra

--- playground/spatial_test.r
+++ playground/spatial_test.r
@@ -1,3 +1,6 @@
+## Works:
+## ==================================================
+
 ## Ardrac
 test <- c14bazAAR::get_aDRAC()
 test2 <- c14bazAAR::determine_country_by_coordinate(test)
@@ -14,5 +17,22 @@ test3[1:5, c("lat","lon","country","country_thes","country_coord","coord_precisi
 test4 <- c14bazAAR::finalize_country_name(test3)
 test4[1:5, c("lat","lon","country","country_thes","country_coord","coord_precision","country_final")]
 
+## CONTEXT
+test <- c14bazAAR::get_CONTEXT()
+test2 <- c14bazAAR::determine_country_by_coordinate(test)
+test3 <- c14bazAAR::standardize_country_name(test2)
+test3[1:5, c("lat","lon","country","country_thes","country_coord","coord_precision")]
+test4 <- c14bazAAR::finalize_country_name(test3)
+test4[1:5, c("lat","lon","country","country_thes","country_coord","coord_precision","country_final")]
 
+## Has issues:
+## ==================================================
 
+## EUROEVOL
+## Problem: coastal distance and too low resolution of rworldxtra data; st_is_within_distance might be suitable to solve the issue. 
+test <- c14bazAAR::get_EUROEVOL()
+test2 <- c14bazAAR::determine_country_by_coordinate(test)
+test3 <- c14bazAAR::standardize_country_name(test2)
+test3[1:5, c("lat","lon","country","country_thes","country_coord","coord_precision")]
+test4 <- c14bazAAR::finalize_country_name(test3)
+test4[1:5, c("lat","lon","country","country_thes","country_coord","coord_precision","country_final")]
