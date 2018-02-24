0f271d6cf201513b86c909e83fc7147cc037e8a2
nevrome <clemens@nevrome.de>
2018-02-24 19:20:31 +0100

    working version of remove_duplicates (see #5)

--- R/c14_date_list_duplicates_remove.R
+++ R/c14_date_list_duplicates_remove.R
@@ -1,10 +1,9 @@
 #### remove_duplicates ####
 
 #' @name remove_duplicates
-#' @title mark duplicates
+#' @title remove duplicates
 #'
-#' @description remove double entries in a c14_date_list by
-#' comparing the Labcodes
+#' @description remove double entries in a c14_date_list
 #'
 #' @param x an object of class c14_date_list
 #'
@@ -32,31 +31,63 @@ remove_duplicates.c14_date_list <- function(x) {
   # start message:
   message(paste0("Removing duplicates... ", {if (nrow(x) > 1000) {"This may take several minutes."}}))
 
-  message("-> Search for accordances in Lab Codes...")
+  # get all unique dates
+  not_duplicates <- x %>%
+    dplyr::filter(
+      is.na(.data$duplicate_group)
+    )
 
-  x %>%
+  # get all duplicates
+  duplicates <- x %>%
     dplyr::filter(
       !is.na(.data$duplicate_group)
-    ) %>%
+    )
+
+  # stringify variation in duplicates: log string
+  stringified_differences <- duplicates %>%
+    plyr::dlply("duplicate_group") %>%
+    purrr::map_chr(.f = stringify_data_frame)
+
+  # combine the duplicates and add the log string
+  summarised_duplicates <- duplicates %>%
     dplyr::group_by(.data$duplicate_group) %>%
     dplyr::summarise_all(
       .funs = dplyr::funs(compare_and_combine(.))
+    ) %>%
+    dplyr::mutate(
+      duplicate_remove_log = stringified_differences
     )
 
-  return(x)
+  # put not_duplicates and duplicates again together
+  not_duplicates$duplicate_remove_log <- NA
+  final_without_duplicates <- not_duplicates %>%
+    rbind(summarised_duplicates)
 
+  return(final_without_duplicates)
 }
 
 #### helper functions ####
 
+stringify_data_frame <- function(x) {
+  # remove all columns that are not character or numeric
+  y <- x[, sapply(x, class) %in% c("character", "numeric", "double", "integer", "factor")]
+  # create string representing the content of the data.frame
+  sapply(colnames(y), function(x) {
+    paste0(x, ": ", paste(unique(y[[x]]), collapse = "|")) }
+  ) %>%
+    paste(collapse = ", ") %>%
+    return()
+}
+
 compare_and_combine <- function(x) {
   # remove NA values
   y <- x[!is.na(x)]
-  # if nothing else present, than return NA
+  # if only NA, than return NA
   if(length(y) == 0) { return(x[1]) }
   # if all values are the same, than return this value
   if(all(y[1] == y)) {
     return(y[1])
+  # else return NA
   } else {
     if(class(y) == "character") {
       return(NA_character_)
@@ -64,6 +95,4 @@ compare_and_combine <- function(x) {
       return(NA)
     }
   }
-  # # else return 3
-  # return(NA)
 }
