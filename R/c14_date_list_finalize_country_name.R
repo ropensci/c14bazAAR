a5f87d123952437751f63b7c24b8a19b341121ae
nevrome <clemens@nevrome.de>
2018-03-03 21:08:30 +0100

    simpler implementation of column availability check

--- R/c14_date_list_finalize_country_name.R
+++ R/c14_date_list_finalize_country_name.R
@@ -28,53 +28,45 @@ finalize_country_name.default <- function(x) {
 #' @export
 finalize_country_name.c14_date_list <- function(x) {
 
-  # check if the columns country_coord and country_thes are present
-  necessary_vars <- c("country_coord", "country_thes")
-  present <- necessary_vars %in% colnames(x)
+  x %>% check_if_columns_are_present(c("country_coord", "country_thes"))
 
-  if (all(present)) {
-    x %<>% dplyr::mutate(ID=seq(1,nrow(x),1))
+  x %<>% dplyr::mutate(ID=seq(1,nrow(x),1))
 
-    sf_x_problem <- x %>%
-      dplyr::filter(is.na(.data$country_coord), !is.na(.data$lat), !is.na(.data$lon)) %>%
-      sf::st_as_sf(coords = c("lon","lat"),
-                   remove = FALSE,
-                   crs = 4326) %>%
-      sf::st_buffer(dist = .5) %>%
-      sf::st_join(y = world) %>%
-      dplyr::mutate(country_coord = as.character(.data$ADMIN.1)) %>%
-      dplyr::select(names(x))
+  world <- get_world_map()
 
-    sf_x_problem <- sf_x_problem[which(sf_x_problem$country_coord == sf_x_problem$country_thes),]
+  sf_x_problem <- x %>%
+    dplyr::filter(is.na(.data$country_coord), !is.na(.data$lat), !is.na(.data$lon)) %>%
+    sf::st_as_sf(coords = c("lon","lat"),
+                 remove = FALSE,
+                 crs = 4326) %>%
+    sf::st_buffer(dist = .5) %>%
+    sf::st_join(y = world) %>%
+    dplyr::mutate(country_coord = as.character(.data$ADMIN.1)) %>%
+    dplyr::select(names(x))
 
-    x <- x %>%
-      dplyr::filter(!is.element(.data$ID, sf_x_problem$ID)) %>%
-      dplyr::bind_rows(., sf_x_problem) %>%
-      dplyr::arrange(.data$ID) %>%
-      dplyr::select(-.data$ID) %>%
-      as.c14_date_list()
-  }
+  sf_x_problem <- sf_x_problem[which(sf_x_problem$country_coord == sf_x_problem$country_thes),]
 
-  if (all(present)) {
-    # add the column country_final
-    x %<>% add_or_replace_column_in_df("country_final", NA, .after = "country_thes")
+  x <- x %>%
+    dplyr::filter(!is.element(.data$ID, sf_x_problem$ID)) %>%
+    dplyr::bind_rows(., sf_x_problem) %>%
+    dplyr::arrange(.data$ID) %>%
+    dplyr::select(-.data$ID) %>%
+    as.c14_date_list()
 
-    # pick the most likely result hierarchically from country_coord, country_thes
-    # and the original input data
-    x %<>%
-      dplyr::mutate(
-        country_final = ifelse(test = !is.na(.data$country_coord),
-                               yes = .data$country_coord,
-                               no = ifelse(test = !is.na(.data$country_thes),
-                                           yes = .data$country_thes,
-                                           no = .data$country))) %>%
-      as.c14_date_list()
-    return(x)
+  # add the column country_final
+  x %<>% add_or_replace_column_in_df("country_final", NA, .after = "country_thes")
 
-  } else {
-    stop(
-      "The following variables (columns) are missing: ",
-      paste(necessary_vars[!present], collapse = ", ")
-    )
-  }
+  # pick the most likely result hierarchically from country_coord, country_thes
+  # and the original input data
+  x %<>%
+    dplyr::mutate(
+      country_final = ifelse(test = !is.na(.data$country_coord),
+                             yes = .data$country_coord,
+                             no = ifelse(test = !is.na(.data$country_thes),
+                                         yes = .data$country_thes,
+                                         no = .data$country))) %>%
+    as.c14_date_list()
+
+  return(x)
+
 }
