bee95a357af6ab6ec9a84354eb6760c84b7a13d8
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2018-03-13 10:53:18 +0100

    added tests for calibrate

---
+++ tests/testthat/test_c14_date_list_calibrate.R
@@ -0,0 +1,38 @@
+context("basic c14_date_calibrate functions")
+
+correct_input <- as.c14_date_list(data.frame(c14age = c(2000, 2500), c14std = c(20, 30)))
+wrong_input <- data.frame(holland="in not")
+
+#### input sensitivity ####
+test_that("calibrate.c14_date_list produces an error with incorrect input", {
+  expect_error(calibrate(wrong_input), "not an object of class c14_date_list")
+})
+
+test_that("calibrate.c14_date_list not produces an error with correct input", {
+  expect_error(calibrate(correct_input), NA)
+})
+
+test_that("calibrate.c14_date_list returns c14_date_list", {
+  expect_true(is.c14_date_list(calibrate(correct_input)))
+})
+
+#### parameter sensitivity ####
+
+test_that("calibrate.c14_date_list correctly considers parameters", {
+  expect_true(all(c("calrange", "sigma") %in% colnames(calibrate(correct_input))))
+  expect_false("calprobdistr" %in% colnames(calibrate(correct_input)))
+  expect_true("calprobdistr" %in% colnames(calibrate(correct_input, choices = "calprobdistr")))
+  expect_false(all(c("calrange", "sigma") %in% colnames(calibrate(correct_input, choices = "calprobdistr"))))
+  expect_true(all(c("calrange", "sigma", "calprobdistr") %in% colnames(calibrate(correct_input, choices = c("calprobdistr", "calrange")))))
+  expect_true(all(calibrate(correct_input, sigma = 3)$sigma==3))
+  expect_true(all(calibrate(correct_input)$sigma==2))
+})
+
+#### Sigma Ranges ####
+
+test_that("calibrate.c14_date_list 2 sigma range is not wider than 3 sigma range", {
+  a <- calibrate(correct_input)$calrange[[1]][,c("from", "to")]
+  b <- calibrate(correct_input, sigma = 3)$calrange[[1]][,c("from", "to")]
+  expect_true(min(a)>=min(b))
+  expect_true(max(a)<=max(b))
+})
