dd5edf06fd39bd3a0c1cc9aad56a3de9331d96b4
nevrome <clemens@nevrome.de>
2018-07-12 15:04:24 +0200

    KITEeastafrica replaced readr::read_csv by data.table::fread

--- R/get_eastafrica.R
+++ R/get_eastafrica.R
@@ -17,42 +17,43 @@ get_KITEeastAfrica <- function(db_url = get_db_url("KITEeastAfrica")) {
 
   # read data
   KITEeastafrica <- tempo %>%
-    readr::read_csv(
+    data.table::fread(
       skip = 3,
-      trim_ws = TRUE,
-      locale = readr::locale(encoding = "ISO-8859-1"),
-      col_types = readr::cols(
-        "Lab Number" = readr::col_character(),
-        "Field Number" = "_", # `= "_"` removes/skips the column
-        "Material Dated" = readr::col_character(),
-        "Taxa Dated" = readr::col_character(),
-        "Type of Date" = "_",
-        "Locality" = "_",
-        "Latitude" = readr::col_character(),
-        "Longitude" = readr::col_character(),
-        "Map Sheet" = "_",
-        "Elevation     (m ASL)" = "_",
-        "Submitter" = "_",
-        "Date Submitted" = "_",
-        "Collector" = "_",
-        "Date Collected" = "_",
-        "Updater" = "_",
-        "Date Updated" = "_",
-        "Measured Age" = "_",
-        "MA Sigma" = "_",
-        "Normalized Age" = readr::col_character(),
-        "NA Sigma" = readr::col_character(),
-        "Delta 13C (per mil)" = readr::col_character(),
-        "Delta 13 C Source" = "_",
-        "Significance" = "_",
-        "Site Identifier" = readr::col_character(),
-        "Site Name" = readr::col_character(),
-        "Stratigraphic Component" = "_",
-        "Context" = "_",
-        "Associated Taxa" = "_",
-        "Additional Information" = readr::col_character(),
-        "Comments" = readr::col_character(),
-        "References" = readr::col_character()
+      encoding = "Latin-1",
+      drop = c(
+        "Field Number",
+        "Type of Date",
+        "Locality",
+        "Map Sheet",
+        "Elevation     (m ASL)",
+        "Submitter",
+        "Date Submitted",
+        "Collector",
+        "Date Collected",
+        "Updater",
+        "Date Updated",
+        "Measured Age",
+        "MA Sigma",
+        "Delta 13 C Source",
+        "Significance",
+        "Stratigraphic Component",
+        "Context",
+        "Associated Taxa"
+      ),
+      colClasses = c(
+        "Lab Number" = "character",
+        "Material Dated" = "character",
+        "Taxa Dated" = "character",
+        "Latitude" = "character",
+        "Longitude" = "character",
+        "Normalized Age" = "character",
+        "NA Sigma" = "character",
+        "Delta 13C (per mil)" = "character",
+        "Site Identifier" = "character",
+        "Site Name" = "character",
+        "Additional Information" = "character",
+        "Comments" = "character",
+        "References" = "character"
       )
     ) %>%
     dplyr::transmute(
@@ -67,7 +68,7 @@ get_KITEeastAfrica <- function(db_url = get_db_url("KITEeastAfrica")) {
       lon = .data[["Longitude"]],
       shortref = .data[["References"]],
       feature = .data[["Site Name"]],
-      comment = paste0(.data[["Comments"]], ",", .data[["Additional Information"]])
+      comment = gsub("^, $", NA, paste0(.data[["Comments"]], .data[["Additional Information"]], sep = ", "))
     ) %>% dplyr::mutate(
       sourcedb = "KITEeastafrica"
     ) %>%
