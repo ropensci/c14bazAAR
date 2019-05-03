a9f4296b5e68089238e1ac843a2a53db2b07f803
nevrome <clemens@nevrome.de>
2019-05-03 15:57:35 +0200

    donwload additional sites table

--- R/get_palmisano.R
+++ R/get_palmisano.R
@@ -2,52 +2,81 @@
 #' @export
 # get_Palmisano <- function(db_url = get_db_url("Palmisano")) {
 
-  # db_url <- "http://discovery.ucl.ac.uk/1575442/1/Palmisanoetal.zip"
-  #
-  # check_connection_to_url(db_url)
-  #
-  # # download and unzip data
-  # temp <- tempfile()
-  # utils::download.file(db_url, temp, quiet = TRUE)
-  # radiocarbon <- utils::unzip(
-  #   temp,
-  #   file.path(
-  #     "Palmisanoetal", "csv", "radiocarbon.csv"
-  #   ),
-  #   exdir = tempdir()
-  # )
-  # sites <- utils::unzip(
-  #   temp,
-  #   file.path(
-  #     "Palmisanoetal", "csv", "sites.csv"
-  #   ),
-  #   exdir = tempdir()
-  # )
-  #
-  # # read data
-  # Palmisano_raw <- data.table::fread(
-  #   radiocarbon,
-  #   sep = ",",
-  #   encoding = "Latin-1",
-  #   drop = c(
-  #     "DateID",
-  #     "LocQual"
-  #   ),
-  #   colClasses = c(
-  #     "LabID" = "character",
-  #     "CRA" = "character",
-  #     "Error" = "character",
-  #     "Material" = "character",
-  #     "Species" = "character",
-  #     "SiteID" = "character",
-  #     "SiteName" = "character",
-  #     "Longitude" = "character",
-  #     "Latitude" = "character",
-  #     "Source" = "character"
-  #   ),
-  #   showProgress = FALSE
-  # )
-  # unlink(temp)
-  # file.remove(radiocarbon)
+  db_url <- "http://discovery.ucl.ac.uk/1575442/1/Palmisanoetal.zip"
+
+  check_connection_to_url(db_url)
+
+  # download and unzip data
+  temp <- tempfile()
+  utils::download.file(db_url, temp, quiet = TRUE)
+  radiocarbon_path <- utils::unzip(
+    temp,
+    file.path(
+      "Palmisanoetal", "csv", "radiocarbon.csv"
+    ),
+    exdir = tempdir()
+  )
+  sites_path <- utils::unzip(
+    temp,
+    file.path(
+      "Palmisanoetal", "csv", "sites.csv"
+    ),
+    exdir = tempdir()
+  )
+
+  # read data
+  radiocarbon_raw <- data.table::fread(
+    radiocarbon_path,
+    sep = ",",
+    encoding = "Latin-1",
+    drop = c(
+      "DateID",
+      "LocQual"
+    ),
+    colClasses = c(
+      "LabID" = "character",
+      "CRA" = "character",
+      "Error" = "character",
+      "Material" = "character",
+      "Species" = "character",
+      "SiteID" = "character",
+      "SiteName" = "character",
+      "Longitude" = "character",
+      "Latitude" = "character",
+      "Source" = "character"
+    ),
+    showProgress = FALSE
+  )
+
+  sites_raw <- data.table::fread(
+    sites_path,
+    sep = ",",
+    encoding = "Latin-1",
+    drop = c(
+      "Toponyms",
+      "Longitude",
+      "Latitude",
+      "LocQual",
+      "SizeHa",
+      "SizeQual",
+      "Source",
+      "Source_id"
+    ),
+    colClasses = c(
+      "Id" = "character",
+      "Type" = "character",
+      "Period" = "character",
+      "StartDate" = "character",
+      "EndDate" = "character"
+    ),
+    showProgress = FALSE
+  )
+
+  # remove files in file system
+  unlink(temp)
+  file.remove(radiocarbon_path)
+  file.remove(sites_path)
+
+  # merge data
 
 # }
