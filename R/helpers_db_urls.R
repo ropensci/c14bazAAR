4d28c94a3429f128f6f651813cda4318ebbed845
Dirk Seidensticker <dirk.seidensticker@gmail.com>
2019-06-25 10:01:02 +0200

    testing candidate for get_EUBAR() parser

--- R/helpers_db_urls.R
+++ R/helpers_db_urls.R
@@ -10,8 +10,8 @@ get_db_url <- function(db_name) {
 
   # URL
   #ref_url <- "https://raw.githubusercontent.com/ISAAKiel/c14bazAAR/master/data-raw/url_reference.csv"
-  ref_url <- "data-raw/url_reference.csv"
-  #check_connection_to_url(ref_url)
+  ref_url <- "https://raw.githubusercontent.com/ISAAKiel/c14bazAAR/EUBAR/data-raw/url_reference.csv"
+  check_connection_to_url(ref_url)
 
   # download current version of reference table
   url_table <- data.table::fread(
