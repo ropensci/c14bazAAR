f41fbb3c7c590b1ad11381842f2e326a87f4cad6
nevrome <clemens@nevrome.de>
2019-06-27 15:29:59 +0200

    removed non-ascii strings from example dataset

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -26,6 +26,7 @@ usethis::use_data(variable_reference, overwrite = TRUE)
 
 all_dates <- c14bazAAR::get_all_dates()
 example_c14_date_list <- all_dates %>%
-  dplyr::sample_n(1000)
+  dplyr::sample_n(1000) %>%
+  dplyr::mutate_if(is.character, function(x) {gsub("[^\x20-\x7E]", "", x)})
 usethis::use_data(example_c14_date_list, overwrite = TRUE)
 
