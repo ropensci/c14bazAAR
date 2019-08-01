32e299baf60280e86f6188ee03886ecad946a1fe
Clemens Schmid <clemens@nevrome.de>
2019-08-01 14:16:18 +0200

    changed as.sf test to consider the new warning

--- tests/testthat/test_c14_date_list_convert.R
+++ tests/testthat/test_c14_date_list_convert.R
@@ -2,7 +2,14 @@ context("conversion functions")
 
 #### as.sf ####
 
-result <- as.sf(example_c14_date_list)
+test_that("as.sf prints a warning when it has to remove dates without coordinates", {
+  expect_warning(
+    as.sf(example_c14_date_list),
+    "Dates without coordinates were removed."
+  )
+})
+
+result <- as.sf(example_c14_date_list, quiet = T)
 
 test_that("as.sf gives back a sf object", {
   expect_s3_class(
