f1fe6ab00f9ae0bf38f52aa683a45f3c6f1875c3
Clemens Schmid <clemens@nevrome.de>
2019-08-01 14:11:11 +0200

    added tests for the duplicate removal options

--- tests/testthat/test_c14_date_list_duplicates.R
+++ tests/testthat/test_c14_date_list_duplicates.R
@@ -67,3 +67,64 @@ test_that("remove_duplicates alone gives the same result as the other functions 
     result3
   )
 })
+
+#### remove_duplicates options ####
+
+option_test_input <- tibble::tribble(
+  ~sourcedb, ~labnr,  ~c14age, ~c14std,
+  "A",       "lab-1", 1100,    10,
+  "A",       "lab-1", 2100,    20,
+  "B",       "lab-1", 3100,    30,
+  "A",       "lab-2", NA,      10,
+  "B",       "lab-2", 2200,    20,
+  "C",       "lab-3", 1300,    10
+) %>%
+  as.c14_date_list() %>%
+  mark_duplicates()
+
+# option 1
+option_test_res_1 <- tibble::tribble(
+  ~sourcedb, ~labnr,  ~c14age, ~c14std,
+  "C",       "lab-3", 1300,    10,
+  NA,        "lab-1", NA,      NA,
+  NA,        "lab-2", 2200,    NA
+) %>% as.c14_date_list()
+
+test_that("duplicate removal option 1 works as expected.", {
+  expect_equal(
+    option_test_input %>% remove_duplicates(., log = FALSE),
+    option_test_res_1
+  )
+})
+
+# option 2
+option_test_res_2 <- tibble::tribble(
+  ~sourcedb, ~labnr,  ~c14age, ~c14std,
+  "A",       "lab-1", 1100,    10,
+  "A",       "lab-2", NA,      10
+) %>% as.c14_date_list()
+
+test_that("duplicate removal option 2 works as expected.", {
+  expect_equal(
+    option_test_input %>% remove_duplicates(
+      preferences = c("A", "B"), log = FALSE
+    ),
+    option_test_res_2
+  )
+})
+
+# option 3
+option_test_res_3 <- tibble::tribble(
+  ~labnr,  ~c14age, ~c14std,
+  "lab-1", 1100,    10,
+  "lab-2", 2200,    10
+) %>% as.c14_date_list()
+
+test_that("duplicate removal option 3 works as expected.", {
+  expect_equal(
+    option_test_input %>% remove_duplicates(
+      preferences = c("A", "B"), supermerge = TRUE, log = FALSE
+    ),
+    option_test_res_3
+  )
+})
