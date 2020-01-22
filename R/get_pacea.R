68b643a40cf5e4884a4a7b938e2a7fa5d9f801c9
David Matzig <david_1@online.de>
2020-01-22 16:35:00 +0100

    started adding data from PACEA database

---
+++ R/get_pacea.R
@@ -0,0 +1,58 @@
+#' @rdname db_getter_backend
+#' @export
+get_radon <- function(db_url = get_db_url("pacea")) {
+
+  check_connection_to_url(db_url)
+
+  # read data
+  pacea <- db_url %>%
+    data.table::fread(
+      quote = "",
+      colClasses = c(
+        ID = "character",
+        LABNR = "character",
+        C14AGE = "character",
+        C14STD = "character",
+        C13 = "character",
+        MATERIAL = "character",
+        SPECIES = "character",
+        COUNTRY = "character",
+        SITE = "character",
+        PERIOD = "character",
+        CULTURE = "character",
+        FEATURETYPE = "character",
+        FEATURE = "character",
+        LATITUDE = "character",
+        LONGITUDE = "character",
+        REFERENCE = "character"
+      ),
+      showProgress = FALSE
+    ) %>%
+    base::replace(., . == "", NA) %>%
+    base::replace(., . == "n/a", NA) %>%
+    base::replace(., . == "n.a.", NA) %>%
+    dplyr::transmute(
+      labnr = .data[["LABNR"]],
+      c14age = .data[["C14AGE"]],
+      c14std = .data[["C14STD"]],
+      c13val = .data[["C13"]],
+      material = .data[["MATERIAL"]],
+      species = .data[["SPECIES"]],
+      country = .data[["COUNTRY"]],
+      site = .data[["SITE"]],
+      period = .data[["PERIOD"]],
+      culture = .data[["CULTURE"]],
+      sitetype = .data[["FEATURETYPE"]],
+      feature = .data[["FEATURE"]],
+      lat = .data[["LATITUDE"]],
+      lon = .data[["LONGITUDE"]],
+      shortref = .data[["REFERENCE"]]
+    ) %>%
+    dplyr::mutate(
+      sourcedb = "radon",
+      sourcedb_version = get_db_version("radon")
+    ) %>%
+    as.c14_date_list()
+
+  return(radon)
+}
