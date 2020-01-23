205973d51e4d696280de463c2dc8c153f2273f75
David Matzig <david_1@online.de>
2020-01-23 12:22:26 +0100

    pacea getter function. Line 7: db_url has to be changed once pull request has been approved

--- R/get_pacea.R
+++ R/get_pacea.R
@@ -1,58 +1,88 @@
 #' @rdname db_getter_backend
 #' @export
-get_radon <- function(db_url = get_db_url("pacea")) {
+get_pacea <- function(db_url = get_db_url("pacea")) {
 
   check_connection_to_url(db_url)
 
+  # db_url <-"http://www.paleoanthro.org/media/journal/content/PA20110001_S01.zip"
+
+  # download and unzip data
+  temp <- tempfile()
+  utils::download.file(db_url, temp, quiet = TRUE)
+  db_path <- utils::unzip(
+    temp,
+    file.path("PA20110001_S01.txt"),
+    exdir = tempdir()
+  )
+
   # read data
-  pacea <- db_url %>%
-    data.table::fread(
-      quote = "",
-      colClasses = c(
-        ID = "character",
-        LABNR = "character",
-        C14AGE = "character",
-        C14STD = "character",
-        C13 = "character",
-        MATERIAL = "character",
-        SPECIES = "character",
-        COUNTRY = "character",
-        SITE = "character",
-        PERIOD = "character",
-        CULTURE = "character",
-        FEATURETYPE = "character",
-        FEATURE = "character",
-        LATITUDE = "character",
-        LONGITUDE = "character",
-        REFERENCE = "character"
-      ),
-      showProgress = FALSE
-    ) %>%
+  db_raw <- data.table::fread(
+    db_path,
+    sep = "\t",
+    encoding = "UTF-8",
+    skip = 1,
+    colClasses = c(
+      "Site" = "character",
+      "Longitude" = "character",
+      "Latitude" = "character",
+      "Town" = "character",
+      "Region" = "character",
+      "Country" = "character",
+      "Biogeography" = "character",
+      "Altitude" = "character",
+      "Orientation" = "character",
+      "Site Type" = "character",
+      "Site Function" = "character",
+      "Cultural Attribution 1" = "character",
+      "Cultural Attribution 2" = "character",
+      "Cultural Attribution 3" = "character",
+      "Level" = "character",
+      "Date Type" = "character",
+      "Sample" = "character",
+      "Code" = "character",
+      "Age" = "character",
+      "s.dev." = "character",
+      "Cal BP" = "character",
+      "cal s.dev." = "character",
+      "IntCal09 max BP" = "character",
+      "IntCal09 min BP" = "character",
+      "Biblio" = "character",
+      "Notes" = "character"
+    ),
+    showProgress = FALSE
+  )
+
+  # remove files in file system
+  unlink(temp)
+  file.remove(db_path)
+
+
+  # final data preparation
+  pacea <- db_raw %>%
     base::replace(., . == "", NA) %>%
-    base::replace(., . == "n/a", NA) %>%
-    base::replace(., . == "n.a.", NA) %>%
     dplyr::transmute(
-      labnr = .data[["LABNR"]],
-      c14age = .data[["C14AGE"]],
-      c14std = .data[["C14STD"]],
-      c13val = .data[["C13"]],
-      material = .data[["MATERIAL"]],
-      species = .data[["SPECIES"]],
-      country = .data[["COUNTRY"]],
-      site = .data[["SITE"]],
-      period = .data[["PERIOD"]],
-      culture = .data[["CULTURE"]],
-      sitetype = .data[["FEATURETYPE"]],
-      feature = .data[["FEATURE"]],
-      lat = .data[["LATITUDE"]],
-      lon = .data[["LONGITUDE"]],
-      shortref = .data[["REFERENCE"]]
-    ) %>%
-    dplyr::mutate(
-      sourcedb = "radon",
-      sourcedb_version = get_db_version("radon")
+      method = .data[["Date Type"]],
+      labnr = .data[["Code"]],
+      c14age = .data[["Age"]],
+      c14std = .data[["s.dev."]],
+      site = .data[["Site"]],
+      sitetype = .data[["Site Type"]],
+      feature = .data[["Site Function"]],
+      period = .data[["Cultural Attribution 1"]],
+      culture = .data[["Cultural Attribution 2"]],
+      material = .data[["Sample"]],
+      region = .data[["Region"]],
+      country = .data[["Country"]],
+      lat = .data[["Latitude"]],
+      lon = .data[["Longitude"]],
+      shortref = .data[["Biblio"]],
+      comment = .data[["Notes"]]
+    ) %>% dplyr::mutate(
+      sourcedb = "pacea",
+      sourcedb_version = get_db_version("pacea")
     ) %>%
     as.c14_date_list()
 
-  return(radon)
+
+  return(pacea)
 }
