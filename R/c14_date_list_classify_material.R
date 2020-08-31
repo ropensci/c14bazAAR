30ec7d687182b2bd047ca46a50ff4cba62760b23
Clemens Schmid <clemens@nevrome.de>
2020-08-31 23:35:06 +0200

    work on catching the last nasty classification problems

--- R/c14_date_list_classify_material.R
+++ R/c14_date_list_classify_material.R
@@ -80,9 +80,13 @@ classify_material.c14_date_list <- function(
 #' @keywords internal
 #' @noRd
 lookup_in_thesaurus_table <- function(x, thesaurus_df){
+  x_cleaned <- x
+  x_cleaned <- gsub("\"", "", x)
+  thesaurus_df$var_cleaned <- thesaurus_df$var
+  thesaurus_df$var_cleaned <- gsub("\"", "", thesaurus_df$var_cleaned)
   ifelse(
-    x %in% thesaurus_df$var,
-    thesaurus_df$cor[match(x, thesaurus_df$var)],
+    x_cleaned %in% thesaurus_df$var_cleaned,
+    thesaurus_df$cor[match(x, thesaurus_df$var_cleaned)],
     x
   ) %>%
     return()
