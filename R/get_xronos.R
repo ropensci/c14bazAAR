e1d59d25b70f869fc3387e82293513774e99ffc8
Clemens Schmid <nevromecs@gmail.com>
2025-01-15 17:49:38 +0100

    mapping two more variables for xronos - requires faster JSON parsing

--- R/get_xronos.R
+++ R/get_xronos.R
@@ -4,9 +4,12 @@ get_xronos <- function(db_url = get_db_url("xronos")) {
 
   check_connection_to_url("https://www.xronos.ch")
 
-  resp<-httr::GET(db_url)
+  resp <- httr::GET(db_url)
 
-  xronos_data <- httr::content(resp,as="parsed") %>% unique() %>% tidyr::tibble()
+  xronos_data <- httr::content(resp, as = "parsed") %>% unique() %>% tidyr::tibble()
+
+  periods_parsed <- parse_json_lists(xronos_data[["periods"]])
+  reference_parsed <- parse_json_lists(xronos_data[["reference"]])
 
   xronos <- xronos_data %>%
     dplyr::transmute(
@@ -20,6 +23,8 @@ get_xronos <- function(db_url = get_db_url("xronos")) {
       site = .data[["site"]],
       sitetype = .data[["site_type"]],
       feature = .data[["feature"]],
+      period = periods_parsed,
+      shortref = reference_parsed,
       lat = .data[["lat"]],
       lon = .data[["lng"]]) %>%
     dplyr::mutate(
@@ -29,4 +34,18 @@ get_xronos <- function(db_url = get_db_url("xronos")) {
     c14bazAAR::as.c14_date_list()
 
   return(xronos)
+}
+
+# helper function to parse JSON to a ;-separated string list
+parse_json_lists <- function(y) {
+  purrr::map_chr(y,
+    \(x) {
+      if (x != "[]") {
+        jsonlite::fromJSON(x, simplifyDataFrame = FALSE) %>%
+          purrr::map(\(x) ifelse(x == "", NA_character_, x)) %>%
+          purrr::reduce(\(x,y) { stringr::str_flatten(c(x,y), collapse = ";", na.rm = T)}) %>%
+          unlist()
+      } else ""
+    }
+  )
 }
